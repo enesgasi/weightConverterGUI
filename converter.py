52e62c4fd91d2ef209f5d0281ac5e62978e5c5fe
enesgasi <enesgasi@hotmail.com>
2023-02-19 12:36:25 +0300

    added lbs

--- converter.py
+++ converter.py
@@ -6,23 +6,27 @@ def from_kg():
     gram = float(e2_value.get())*1000
     pound = float(e2_value.get())*2.20462
     ounce = float(e2_value.get())*35.274
+
+
     t1.delete("1.0",END)
     t1.insert(END, gram)
     t2.delete("1.0", END)
     t2.insert(END, pound)
     t3.delete("1.0", END)
-    t3.insert(END, ounce)
+    t3.insert(END, ounce)  
+
 
 e1 = Label(window, text="Input the weight in KG")
 e2_value = StringVar()
 e2 = Entry(window, textvariable=e2_value)
 e3 = Label(window, text="Gram")
-e4 = Label(window, text="Pound")
+e4 = Label(window, text="Pound (lbs)")
 e5 = Label(window, text="Ounce")
 
 t1 = Text(window, height=5, width=30)
 t2 = Text(window, height=5, width=30)
-t3 = Text(window, height=5, width=30) 
+t3 = Text(window, height=5, width=30)
+t4 = Text(window, height=5, width=30)  
 
 b1 = Button(window, text='Convert', command=from_kg)
 
